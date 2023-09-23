931cc77fb275dd485169b306ff8ce4d5185ac08d
Adam Gastiev <agast6335@gmail.com>
2023-09-23 16:06:28 +0300

    update tasks_filter

--- src/components/tasks-filter/tasks-filter.js
+++ src/components/tasks-filter/tasks-filter.js
@@ -1,31 +1,43 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import './tasks-filter.css';
 
-export default class TasksFilter extends Component {
-	static defaulProps = {
-		filter: 'ALL',
-	}
-	static propTypes = {
-		filter: PropTypes.string
-	}
-	render() {
-		const { filter } = this.props;
-		return (
-			<ul className="filters">
-				<li>
-					<button onClick={() => this.props.onChangeFilter('ALL')}
-						className={filter === "ALL" ? 'selected' : null}>All</button>
-				</li>
-				<li>
-					<button onClick={() => this.props.onChangeFilter('Active')}
-						className={filter === "Active" ? 'selected' : null}>Active</button>
-				</li>
-				<li>
-					<button onClick={() => this.props.onChangeFilter('Completed')}
-						className={filter === "Completed" ? 'selected' : null}>Completed</button>
-				</li>
-			</ul>
-		);
-	}
+function TasksFilter(props) {
+  const { filter, onChangeFilter } = props;
+  return (
+    <ul className="filters">
+      <li>
+        <button type="button" onClick={() => onChangeFilter('ALL')} className={filter === 'ALL' ? 'selected' : null}>
+          All
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          onClick={() => onChangeFilter('Active')}
+          className={filter === 'Active' ? 'selected' : null}
+        >
+          Active
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          onClick={() => onChangeFilter('Completed')}
+          className={filter === 'Completed' ? 'selected' : null}
+        >
+          Completed
+        </button>
+      </li>
+    </ul>
+  );
 }
+
+TasksFilter.defaulProps = {
+  filter: 'ALL',
+};
+
+TasksFilter.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
+};
+export default TasksFilter;
